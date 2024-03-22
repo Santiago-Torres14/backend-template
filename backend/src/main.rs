ff1253931c2278cf3c6c72bf097fe0782f8812d8
Santiago Torres Aragon <contact@santiagotorres.me>
2024-03-21 20:06:12 -0500

    feat:
    1. set up axum router and bind port.
    2. set up database pool connections.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,40 @@
-use chrono::offset::Utc;
-use serde_json;
+use std::{error::Error, str::FromStr};
 
+use axum::{Router, routing::get};
+use bb8_postgres::{PostgresConnectionManager, bb8::Pool};
+use tokio_postgres::NoTls;
+
+
+#[derive(Clone)]
+struct AppState {
+    pub pg_pool: Pool<PostgresConnectionManager<NoTls>>
+}
+
+type BoxedError = Box<dyn Error + Send + Sync>;
 
 #[tokio::main]
-async fn main() {
-    let now = serde_json::to_string(&Utc::now()).unwrap();
-    println!("{now}");
-}
+async fn main() -> Result<(), BoxedError>{
+
+    dotenv::dotenv().ok();
+
+    let host = std::env::var("DB_HOST").expect("NO ENV HOST SET");
+    let username = std::env::var("DB_USERNAME").expect("NO ENV USERNAME SET");
+    let password = std::env::var("DB_PASSWORD").expect("NO ENV PASSWORD SET");
+    let postgres_config = tokio_postgres::Config::from_str(&format!("postgres://${username}:${password}@${host}:5432"))?;
+    let pg_cm = PostgresConnectionManager::new(postgres_config, NoTls);
+
+    let pool = Pool::builder().build(pg_cm).await?;
+
+
+    let app_state = AppState {
+        pg_pool: pool
+    };
+
+    let router = Router::new().route("/", get(|| async { "hello, world"})).with_state(app_state);
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
+
+    axum::serve(listener, router).await?;
+
+    Ok(())
+} 
